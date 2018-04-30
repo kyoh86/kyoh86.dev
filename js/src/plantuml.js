213b820559b5506687d0ef1b93b9a7768077d672
Kyoichiro Yamada <kyoh86@users.noreply.github.com>
2018-04-30 18:48:39 +0900

    UnSSLfy plantuml

--- js/src/plantuml.js
+++ js/src/plantuml.js
@@ -5,7 +5,7 @@ import {Encoder} from './encoder.js';
 window.addEventListener("load",  function() {
   document.querySelectorAll('code.language-uml').forEach(el => {
     let text = el.textContent;
-    let url = "//plantuml.com/plantuml/img/" + Encoder.compress(text);
+    let url = "http://plantuml.com/plantuml/img/" + Encoder.compress(text);
     el.parentNode.outerHTML = '<div><img src="' + url + '" /></div>';
     console.debug("encoding " + el.textContent);
   });
